70b48bfc74d9ed2b34760935406b9cdd53a8e00f
ag2111 <agoziev@uchicago.edu>
2022-03-08 22:38:44 +0000

    Model Setup Finished

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,4 +16,14 @@ class User < ApplicationRecord
   validates :email, :uniqueness => { :case_sensitive => false }
   validates :email, :presence => true
   has_secure_password
+
+  has_many(:comments, { :class_name => "Comment", :foreign_key => "author_id", :dependent => :destroy })
+  has_many(:follow_requests, { :class_name => "FollowRequest", :foreign_key => "sender_id", :dependent => :destroy })
+  has_many(:get_follower, { :class_name => "FollowRequest", :foreign_key => "recipient_id", :dependent => :destroy })
+  has_many(:likes, { :class_name => "Like", :foreign_key => "fan_id", :dependent => :destroy })
+  has_many(:photos, { :class_name => "Photo", :foreign_key => "owner_id", :dependent => :destroy })
+
+  has_many(:followees, { :through => :follow_requests, :source => :recipient })
+  has_many(:followers, { :through => :get_follower, :source => :sender })
+  
 end
